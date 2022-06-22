0f6b6743ea943a2392db3c3f660286a0aba4e076
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-06-21 23:23:30 -0300

    Home Menu

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,48 @@
+import * as S from './style';
+import { ReactComponent as Search } from '../../assets/icons/search.svg';
+import { DateTime } from 'luxon';
+import Menu from '../../components/Menu';
+import { RoutePath } from '../../types/routes';
+import { navigationItems } from '../../data/navigation';
+
 const Home = () => {
-	return <div>Componente Home</div>;
+	const dateDescription = DateTime.now().toLocaleString({
+		...DateTime.DATE_SHORT,
+		weekday: 'long',
+	});
+
+	return (
+		<S.Home>
+			<Menu active={RoutePath.HOME} navItems={navigationItems} />
+			<S.HomeContent>
+				<header>
+					<S.HomeHeaderDetails>
+						<div>
+							<S.HomeHeaderDetailsLogo>Pizza Fresh</S.HomeHeaderDetailsLogo>
+							<S.HomeHeaderDetailsDate>
+								{dateDescription}
+							</S.HomeHeaderDetailsDate>
+						</div>
+						<S.HomeHeaderDetailsSearch>
+							<Search />
+							<input type="text" placeholder="Procure pelo sabor" />
+						</S.HomeHeaderDetailsSearch>
+					</S.HomeHeaderDetails>
+				</header>
+				<div>
+					<S.HomeProductTitle>
+						<b>Pizzas</b>
+					</S.HomeProductTitle>
+					<S.HomeProductList>
+						<p>Lista de produtos aqui</p>
+					</S.HomeProductList>
+				</div>
+			</S.HomeContent>
+			<aside>
+				<p>Detalhes dos pedidos aqui</p>
+			</aside>
+		</S.Home>
+	);
 };
 
 export default Home;
