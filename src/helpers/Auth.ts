067d2c486f12fd5e8d0c247d1beda9e6a1e9e6be
Jefferson Silva <developer.jeffrock27@outlook.com>
2023-03-23 17:12:02 -0300

    feature Auth

---
+++ src/helpers/Auth.ts
@@ -0,0 +1,17 @@
+import { LocalStorageHelper } from "./LocalStorageHelper";
+import { LocalStorageKeys } from "types/localStorageKeys";
+import { RoutePath } from "types/routes";
+
+
+export const Auth = {
+  isAuth: (): boolean => {
+    const token = LocalStorageHelper.get(LocalStorageKeys.TOKEN)
+    const user = LocalStorageHelper.get(LocalStorageKeys.USER)
+
+    return token !== null && user !== null
+  },
+  logout: (): void => {
+    LocalStorageHelper.clear
+    window.location.replace(RoutePath.LOGIN)
+  }
+}
