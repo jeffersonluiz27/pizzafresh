773d465cbd2e962bd3a1c431cac44bbf68aa24b6
Jefferson Silva <developer.jeffrock27@outlook.com>
2023-03-27 16:39:44 -0300

    feat: crud tables

--- src/components/EditTable/index.tsx
+++ src/components/EditTable/index.tsx
@@ -1,23 +1,60 @@
 import { ReactComponent as Pencil } from 'assets/icons/edit.svg';
+import { useEffect, useState } from 'react';
+import { TableResponse } from 'types/api/table';
 import * as S from './style';
 
-const EditTable = () => {
-	return (
-		<S.EditTable>
-			<>
-				<S.EditTableDetails>
-					<S.EditTableDetailsName>Número da mesa</S.EditTableDetailsName>
-				</S.EditTableDetails>
-				<S.EditTableAction>
-					<Pencil /> Editar
-				</S.EditTableAction>
-			</>
+interface EditTableProps {
+	table: TableResponse;
+	onCancel: boolean;
+	onEdit: (data: TableResponse) => void;
+	onDelete: (data: TableResponse) => void;
+}
 
-			<>
-				<label htmlFor="tableId">Número da mesa</label>
-				<S.EditForm id="tableId" type="number" placeholder="01" />
-				<S.Deletar>Deletar Mesa</S.Deletar>
-			</>
+const EditTable = ({ 
+	table, 
+	onCancel, 
+	onDelete, 
+	onEdit,
+}: EditTableProps) => {
+	const [isEditing, setIsEditing] = useState(false);
+
+	useEffect(() => {
+		setIsEditing(false);
+	}, [onCancel]);
+
+	const onEditClick = () => {
+		setIsEditing(true);
+		onEdit(table);
+	};
+
+	const onEditChange = (tableId: string) => {
+		setIsEditing(true);
+		onEdit({ ...table, number: Number(tableId) });
+	};
+
+	return (
+		<S.EditTable role="listitem">
+			{!isEditing ? (
+				<>
+					<S.EditTableDetails>
+						<S.EditTableDetailsName>{table.number}</S.EditTableDetailsName>
+					</S.EditTableDetails>
+					<S.EditTableAction onClick={() => onEditClick()}>
+						<Pencil /> Editar
+					</S.EditTableAction>
+				</>
+			) : (
+				<>
+					<label htmlFor="tableId">Número da mesa</label>
+					<S.EditForm
+						id="tableId"
+						type="number"
+						placeholder="01"
+						onChange={({ target }) => onEditChange(target.value)}
+					/>
+					<S.Deletar onClick={() => onDelete(table)}>Deletar Mesa</S.Deletar>
+				</>
+			)}
 		</S.EditTable>
 	);
 };
